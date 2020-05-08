4667121d1caea963ddeb94e38e9a25876db45787
shigeakihosogane <xshigeaki@gmail.com>
2020-05-08 18:51:42 +0900

    Customerモデル作成 初回マイグレーション完了

---
+++ Hinode/Hinode/Models/HndContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Hinode.Models
+{
+    public class HndContext : DbContext
+    {
+        // b. コンス トラクター 
+        public HndContext( DbContextOptions <HndContext> options) : base( options) { }
+
+        // c. モデル クラス への アクセサー 
+        public DbSet <Customer> Customer { get; set; }
+
+
+        
+    }
+}
