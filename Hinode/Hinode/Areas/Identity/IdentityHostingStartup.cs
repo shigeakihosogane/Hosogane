b177071e24fe199d8b31dd6cc6a192b40748e0c4
shigeakihosogane <xshigeaki@gmail.com>
2020-05-10 13:53:00 +0900

    IDスキャフォールディング

---
+++ Hinode/Hinode/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hinode.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Hinode.Areas.Identity.IdentityHostingStartup))]
+namespace Hinode.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
